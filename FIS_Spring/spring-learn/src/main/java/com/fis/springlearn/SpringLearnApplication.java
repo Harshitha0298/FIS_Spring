8e1dfcde092dafb6f589bc68471b6f7f4fd7bb3a
Harshitha0298 <harshithagbelagavi98@gmail.com>
2021-08-10 12:49:32 +0530

    added countries

--- FIS_Spring/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ FIS_Spring/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -2,11 +2,11 @@ package com.fis.springlearn;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,9 +18,16 @@ public class SpringLearnApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(SpringLearnApplication.class, args);
 //		displayDate();
-		displayCountry();
+//     displayCountry();
+	   displayCountries();
 	}
-
+	static void displayCountries() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		ArrayList<Country> countries = context.getBean("countryList", ArrayList.class);
+		LOGGER.debug("Countries : {}", countries);
+		LOGGER.info("END");
+	}
 	static void displayCountry() {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
 		Country country = context.getBean("country", Country.class);
