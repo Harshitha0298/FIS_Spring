9f21f2a87690977e9634cf075ecdf471e40a1983
Harshitha0298 <harshithagbelagavi98@gmail.com>
2021-08-10 15:52:47 +0530

    added employee info

--- FIS_Spring/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ FIS_Spring/spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.fis.springlearn.bean.Employee;
+
 @SpringBootApplication
 public class SpringLearnApplication {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
@@ -19,7 +21,15 @@ public class SpringLearnApplication {
 //		SpringApplication.run(SpringLearnApplication.class, args);
 //		displayDate();
 //     displayCountry();
-	   displayCountries();
+//	   displayCountries();
+		displayEmployee();
+	}
+	static void displayEmployee() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
+		Employee employee = context.getBean("employee", Employee.class);
+		LOGGER.debug("Employee : {}", employee);
+		LOGGER.info("END");
 	}
 	static void displayCountries() {
 		LOGGER.info("START");
