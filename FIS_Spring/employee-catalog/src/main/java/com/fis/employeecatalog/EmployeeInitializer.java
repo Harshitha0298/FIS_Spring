2de43785dd4d6a763b35e222e416043ad03a798a
Harshitha0298 <harshithagbelagavi98@gmail.com>
2021-08-14 18:08:28 +0530

    added Employee catalog

--- FIS_Spring/employee-catalog/src/main/java/com/fis/employeecatalog/EmployeeInitializer.java
+++ FIS_Spring/employee-catalog/src/main/java/com/fis/employeecatalog/EmployeeInitializer.java
@@ -13,14 +13,14 @@ class  EmployeeInitializer implements CommandLineRunner {
 
     private final EmployeeRepository employeeRepository;
 
-    EmployeeInitializerEmployeeRepository employeeRepository) {
+    EmployeeInitializer (EmployeeRepository employeeRepository) {
         this.employeeRepository =employeeRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Stream.of("Lining", "PUMA", "Bad Boy", "Air Jordan", "Nike", "Adidas", "Reebok")
-                .forEach(item -> employeeRepository.save(new Employee(employee)));
+        Stream.of("RM", "Jin", "Suga", "J hope", "Jimin", "V", "jung kook")
+                .forEach(item -> employeeRepository.save(new Employee(item)));
 
         employeeRepository.findAll().forEach(System.out::println);
     }
